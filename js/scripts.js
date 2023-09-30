4597cc3ab71b88e22da5e6310a944f960c3139a2
Jason <mat.lopez072023@gmail.com>
2023-09-30 08:46:07 +0800

    added edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+console.log("==================================");
+console.log("Welcome to my JavaScript project!");
+console.log("==================================");
